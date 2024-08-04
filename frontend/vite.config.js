75775b3d6cb71e8da2ddf3046026eb736cf81528
Utkarsh-Singh-Chandel <usc161@gmail.com>
2024-08-04 20:03:16 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       '/api':{
-        target:'https://localhost:5000',
+        target:'http://localhost:5000',
         
       }
     }
